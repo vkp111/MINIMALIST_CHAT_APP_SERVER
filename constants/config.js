a55308720a49a97c334c04aff7be1cb5f39e4e59
Vijayant Pandey <119390705+vkp111@users.noreply.github.com>
2025-01-07 19:31:52 +0530

    Update config.js
    Updated the second Time

--- constants/config.js
+++ constants/config.js
@@ -1,11 +1,11 @@
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://minimalist-chat-app-client.vercel.app/",
-    "https://minimalist-chat-app-client-git-master-vijayant-pandeys-projects.vercel.app/",
-    "https://minimalist-chat-app-client-8g6m548i0-vijayant-pandeys-projects.vercel.app/"
+    "https://minimalist-chat-app-client.vercel.app",
+    "https://minimalist-chat-app-client-git-master-vijayant-pandeys-projects.vercel.app",
+    "https://minimalist-chat-app-client-8g6m548i0-vijayant-pandeys-projects.vercel.app",
     "http://localhost:4173",
-    process.env.CLIENT_URL,
+    process.env.CLIENT_URL, // Make sure this environment variable is correctly set
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
