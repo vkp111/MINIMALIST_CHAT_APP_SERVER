cc547ca24752765088f761c6065e11f29ef01dd5
Vijayant Pandey <119390705+vkp111@users.noreply.github.com>
2025-01-07 19:20:09 +0530

    Update config.js
    Added the vercel frontend Links

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,9 @@
 const corsOptions = {
   origin: [
     "http://localhost:5173",
+    "https://minimalist-chat-app-client.vercel.app/",
+    "https://minimalist-chat-app-client-git-master-vijayant-pandeys-projects.vercel.app/",
+    "https://minimalist-chat-app-client-8g6m548i0-vijayant-pandeys-projects.vercel.app/"
     "http://localhost:4173",
     process.env.CLIENT_URL,
   ],
